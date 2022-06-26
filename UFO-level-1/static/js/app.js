5adda22e595c1dd78972e6f5ebcda6578066bab4
Grant Towers <grant_towers@bigpond.com>
2022-06-26 18:07:36 +0800

    First Draft

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
-// from data.js
-var tableData = data;
+// Part 1 - Create data for the sighting data table:
+// Create variables for the table data and tbody
+var sightingData = data;
+var tbody = d3.select("tbody"); // This is the HTML table item
 
-// YOUR CODE HERE!
+// Append rows and cell per sighting report value (Date, City, State, Country, Shape, Duration and Comments)
+data.forEach(function (sightingData) {
+    var row = tbody.append("tr");
+    Object.entries(sightingData).forEach(function ([key, value]) {
+        // Append a cell to the row for each value in the sighting data object
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Part 2 - Creating the Date Filter function
+// Storing the date input value and creating variable for button
+var form = d3.select("#form");
+var button = d3.select("#filter-btn");
+
+// Create event handlers for clicking the "Filter Table" button or pressing the enter key
+form.on("submit", runEnter);
+button.on("click", runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    var datetime_input = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = datetime_input.property("value");
+    // Print the value to the console
+    console.log(inputValue);
+    console.log(sightingData);
+
+    // Cannot get the filter to work... getting and empty list back
+    var filteredData = sightingData.filter(date => sightingData.datetime === inputValue);
+    console.log(filteredData)
+
+    // Clear out all data in the tbody table - remove data from the table
+    tbody.html("");
+
+    // Append rows and cell per sighting report value (Date, City, State, Country, Shape, Duration and Comments)
+    data.forEach(function (filteredData) {
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(function ([key, value]) {
+            // Append a cell to the row for each value in the sighting data object
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
